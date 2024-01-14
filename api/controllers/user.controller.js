11320812de38d897bf2d118e0bb8df5153e6b0d7
nahid-priom <nahid.priom.06@gmail.com>
2024-01-14 22:08:41 +0600

    create test api route

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,6 @@
+const test = (req, res) => {
+    res.json({
+      message: "Hello World",
+    });
+}
+export default test
--- api/index.js
+++ api/index.js
@@ -1,16 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv"
-dotenv.config()
+import dotenv from "dotenv";
+import useRouter from "./routes/user.route.js";
+dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=>{
-    console.log('Connected to MongoDB')
-})
-.catch((err) => {
-    console.log(err)
-})
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
-app.listen(3000, ()=>{
-    console.log('Server is running on port 3000 !!')
-})
+app.listen(3000, () => {
+  console.log("Server is running on port 3000 !!");
+});
+
+app.use("/api/user", useRouter);
