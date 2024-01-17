87de4fe7d36fb92b4c8501482726edda7fe5da37
nahid-priom <nahid.priom.06@gmail.com>
2024-01-17 23:21:33 +0600

    create sign up api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,3 +1,14 @@
-export const signup = (req, res) => {
-  console.log(req.body);
+import User from "../models/user.models.js";
+import bcryptjs from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json("User Created Succesfully");
+  } catch (error) {
+    res.status(500).json(error.message);
+  }
 };
--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ mongoose
     console.log(err);
   });
 const app = express();
-
+app.use(express.json())
 app.listen(3000, () => {
   console.log("Server is running on port 3000 !!");
 });
