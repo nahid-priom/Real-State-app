9e8fa41b21d27b24c140823cf7fb3700d4947d46
nahid-priom <nahid.priom.06@gmail.com>
2024-01-18 15:40:14 +0600

    create a middleware and a function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -14,10 +14,20 @@ mongoose
     console.log(err);
   });
 const app = express();
-app.use(express.json())
+app.use(express.json());
 app.listen(3000, () => {
   console.log("Server is running on port 3000 !!");
 });
 
 app.use("/api/user", useRouter);
-app.use("/api/auth", authRouter)
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
