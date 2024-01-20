f30636110f2be8ee9c03fc08acfc8f84e27a9d8f
nahid-priom <nahid.priom.06@gmail.com>
2024-01-20 22:53:13 +0600

    update the header and make profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,17 +6,20 @@ import Signin from "./pages/Signin";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import Signup from "./pages/Signup";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Header/>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const { currentUser } = useSelector((state) => state.user);
+
   return (
     <header className="bg-slate-200 shadow-md px-2 sm:px-0">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -31,10 +34,19 @@ const Header = () => {
               About
             </li>
           </Link>
-          <Link to="signin">
-            <li className="inline text-slate-700 hover:underline cursor-pointer">
-              Sign In
-            </li>
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                className="rounded-full h-7 w-7 object-cover"
+                src={currentUser.avater}
+                alt="profile"
+              />
+              
+            ) : (
+              <li className="inline text-slate-700 hover:underline cursor-pointer">
+                Sign In
+              </li>
+            )}
           </Link>
         </ul>
       </div>
