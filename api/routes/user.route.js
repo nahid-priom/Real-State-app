8267831ea0daedf8645916748f5b54296a82910d
nahid-priom <nahid.priom.06@gmail.com>
2024-01-27 23:23:12 +0600

    Add contact to the listing page

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import test, { getUserListings, updateUser } from "../controllers/user.controller.js";
+import test, { getUser, getUserListings, updateUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser)
 router.get('/listings/:id',getUserListings)
+router.get('/:id', getUser)
 
 export default router;
--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -14,6 +14,7 @@ import {
   FaParking,
   FaShare,
 } from 'react-icons/fa';
+import Contact from '../components/Contact';
 
 
 // https://sabe.io/blog/javascript-format-numbers-commas#:~:text=The%20best%20way%20to%20format,format%20the%20number%20with%20commas.
