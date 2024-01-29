6d3006d959cfd350e2bec50395b18ea9f3e19f8e
nahid-priom <nahid.priom.06@gmail.com>
2024-01-29 15:58:49 +0600

    update header search form

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const Header = () => {
   const { currentUser } = useSelector((state) => state.user);
-
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search)
+    const searchTermFromUrl = urlParams.get('searchTerm')
+    if(searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl)
+    }
+  }, [location.search])
   return (
     <header className="bg-slate-200 shadow-md px-2 sm:px-0">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -15,13 +30,20 @@ const Header = () => {
             <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
-        <form className="w-40 md:w-64 xl:w-1/2 bg-slate-100 p-2 rounded-lg flex justify-between items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="w-40 md:w-64 xl:w-1/2 bg-slate-100 p-2 rounded-lg flex justify-between items-center"
+        >
           <input
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <FaSearch className="text-slate-500" />
+          <button>
+            <FaSearch className="text-slate-500" />
+          </button>
         </form>
         <ul className="flex gap-4">
           <Link to="/">
@@ -41,7 +63,6 @@ const Header = () => {
                 src={currentUser.avater}
                 alt="profile"
               />
-              
             ) : (
               <li className="inline text-slate-700 hover:underline cursor-pointer">
                 Sign In
